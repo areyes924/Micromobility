dc8cd5939bde89d22427c099470f7ccc9901ba2e
areyes924 <98560613+areyes924@users.noreply.github.com>
2025-10-20 11:02:16 -0700

    Moved datasets to designated folder

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
-# Import Data
-station_map = pd.read_csv("metro-bike-share-stations-2025-10-01.csv")
-q3_2025 = pd.read_csv("metro-trips-2025-q3.csv")
-q2_2025 = pd.read_csv("metro-trips-2025-q2.csv")
+# ==================================================
+# Data Loading
+# ==================================================
 
-# When testing, it's nice to just focus on one.
-df = q2_2025
+station_map = pd.read_csv("data/raw/metro-bike-share-stations-2025-10-01.csv")
+q3_2025 = pd.read_csv("data/raw/metro-trips-2025-q3.csv")
+q2_2025 = pd.read_csv("data/raw/metro-trips-2025-q2.csv")
+
+df = q2_2025 # When testing, it's nice to just focus on one.
 
 # Store relevant station IDs. We could do this in pandas using station_map
 
@@ -14,6 +16,11 @@ perloff_id = 4614 # North Campus Station near Bunche/Perloff Halls
 drake_id = 4643 # Drake Stadium Station near Epicuria/Bfit
 gateway_id1 = 4613 # Gateway Plaza Station near Bus stops/Pauley Pavilion
 gateway_id2 = 4665 # For some reason, there are two station IDs for gateway plaza. This second one doesn't seem to be in use.
+
+# ==================================================
+# Cleaning
+# ==================================================
+
 
 # Contingency in case Gateway_id2 is used.
 gateway_id2_rides = df[(df["start_station"] == gateway_id2) | (df["end_station"] == gateway_id2)]
@@ -24,7 +31,11 @@ else: print("Gateway ID2 check OK")
 # print (station_map.info())
 # print (q3_2025.info())
 
-# Filter for rides that start at drake_id and end at perloff_id
+# ==================================================
+# Analysis
+# ==================================================
+
+# Filter for rides that start at drake_id and end at gateway_id
 rides = q2_2025[(q2_2025["start_station"] == drake_id) & (q2_2025["end_station"] == gateway_id1)]
 
 # Count the total number of such rides
