f74022bb5316b18b514a6a05642230d561c37f9c
areyes924 <98560613+areyes924@users.noreply.github.com>
2025-11-05 01:58:51 -0800

    Completed Model, with regression summary

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-stations = pd.read_csv("data/raw/metro-bike-share-stations.csv")
-trips = pd.read_csv("data/processed/metro_trips_ucla_24-25.csv")
-
-print("===== Stations dtypes =====")
-print(pd.DataFrame(stations.dtypes, columns=['dtype']))
-print("===== Trips dtypes =====")
-print(pd.DataFrame(trips.dtypes, columns=['dtype']))
